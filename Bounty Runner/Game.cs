de36b9fe2c22f70f4a717f5d780f889f2f25bdee
JamesonPhong <000882371@mohawkcollege.ca>
2025-10-31 12:37:04 -0400

    Add Visual Studio Project

---
+++ Bounty Runner/Game.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Numerics;
+
+namespace MohawkGame2D
+{
+    public class Game
+    {
+        public void Setup()
+        {
+
+        }
+        public void Update()
+        {
+
+        }
+    }
+}
