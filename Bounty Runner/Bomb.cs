c6477df3d97af632c2d187e13e3435aab149fe83
JamesonPhong <000882371@mohawkcollege.ca>
2025-11-03 11:48:15 -0500

    Changed Debris.cs to Bomb.cs

--- Bounty Runner/Bomb.cs
+++ Bounty Runner/Bomb.cs
@@ -2,13 +2,13 @@
 using System.Numerics;
 
 namespace MohawkGame2D;
-public class Debris
+public class Bomb
 {
     Vector2 position;
     Vector2 velocity;
 
     public void Setup()
     {
-        
+        position = new Vector2();
     }
 }
