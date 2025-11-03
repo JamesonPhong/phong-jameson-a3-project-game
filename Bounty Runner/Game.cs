480d1466c86bc47f9a717a090ec72bc9e777ba49
JamesonPhong <000882371@mohawkcollege.ca>
2025-11-03 13:21:45 -0500

    Add Player movement

--- Bounty Runner/Game.cs
+++ Bounty Runner/Game.cs
@@ -5,11 +5,15 @@ namespace MohawkGame2D
 {
     public class Game
     {
-        Bomb[] bombs = new Bomb[5];
+        Bomb[] bombs = new Bomb[1];
+        Vector2 position;
+        Vector2 velocity;
         public void Setup()
         {
             Window.SetTitle("Bounty Runner");
             Window.SetSize(800, 600);
+
+            position = new Vector2(100, 300);
 
             for (int i = 0; i < bombs.Length; i++)
             {
@@ -33,6 +37,7 @@ namespace MohawkGame2D
             }
 
             Player();
+            PlayerGravity();
         }
         void Player()
         {
@@ -40,13 +45,42 @@ namespace MohawkGame2D
             Draw.LineSize = 1;
             Draw.LineColor = Color.Black;
             Draw.FillColor = new Color("FFE9D1");
-            Draw.Circle(175, 325, 25);
+            Draw.Circle(position.X + 75, position.Y + 25, 25);
 
             // Draw player body
             Draw.LineSize = 1;
             Draw.LineColor = Color.Black;
             Draw.FillColor = Color.Red;
-            Draw.Rectangle(150, 350, 50, 100);
+            Draw.Rectangle(position.X + 50, position.Y + 50, 50, 100);
+
+            // Player Jump
+            if (Input.IsKeyboardKeyPressed(KeyboardInput.Up) ||
+                Input.IsKeyboardKeyPressed(KeyboardInput.W))
+            {
+                position.Y -= 105;
+            }
+            // Player Moves Left
+            if (Input.IsKeyboardKeyDown(KeyboardInput.Left) || 
+                Input.IsKeyboardKeyDown(KeyboardInput.A))
+            {
+                position.X -= 10;
+            }
+            // Player Moves Right
+            if (Input.IsKeyboardKeyDown(KeyboardInput.Right) || 
+                Input.IsKeyboardKeyDown(KeyboardInput.D))
+            {
+                position.X += 10;
+            }
+        }
+        void PlayerGravity()
+        {
+            velocity += new Vector2(0,105) * Time.DeltaTime;
+            position += velocity;
+            if (position.Y > 300)
+            {
+                position.Y = 300;
+                velocity.Y = 0;
+            }
         }
     }
 }
